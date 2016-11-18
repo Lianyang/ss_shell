db04b7febca4600a1c93f5be95304d8a66fdf285
conglianyang <conglianyang@gmail.com>
2016-11-18 17:20:15 +0800

    add test file

---
+++ test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+from urllib import urlopen
+from mysql.utilities.common.topology import Topology
+from mysql.utilities.common.replication import Master
+
+
+# text = urlopen('http://www.ishadowsocks.com/').read()
+
+print 'hello'
+
+import pdb; pdb.set_trace()  # breakpoint aa5272cc //
+print 'nihao'
+
+
+import pdb; pdb.set_trace()  # breakpoint 0cb494da //
+print 'nihaoya'
+
---
+++ tools.py
@@ -0,0 +1,49 @@
+#!/bin/env/python
+
+import pycurl
+from mysql.utilities import common
+
+from flask import Flask,session
+
+from HTMLParser import HTMLParser
+
+from urllib2 import urlopen
+
+
+
+class Curl():
+    def __init__(self, url):
+        self.url = url
+
+    def set_post_data(self, post_data):
+
+def set(self):
+    pass
+
+class NewClass(object):
+    """docstring for NewClass
+"""
+    def __init__(self, arg):
+        super(NewClass
+            , self).__init__()
+        self.arg = arg
+
+if __name__ == "__main__":
+    try:
+        pass
+    except Exception as e:
+        raise
+    else:
+        pass
+    finally:
+        pass
+try:
+    pass
+except Exception as e:
+    raise e
+finally:
+    pass
+
+def newfunc(parms):
+    print "hahah"
+    return parms
